ce112d9f50c0305e5f470ec2fe4d19707e04a180
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2023-10-25 22:01:22 -0300

    Se limpio toda la pantalla de inicio de React

---
+++ shop-react-tp4/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Hola from './components/Hola';
+function App() {
+  return (
+    <div className="App">
+      <h1>Shop Web - Decorator</h1>
+      <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
