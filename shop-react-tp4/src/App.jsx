491412d594a04d552685eab156a85c1c5cf9dcc3
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2023-11-02 14:54:00 -0300

    ADD:Temperatura useState

--- shop-react-tp4/src/App.jsx
+++ shop-react-tp4/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Contador from './components/Contador'
 import Listado from './components/Listado'; 
+import Temperatura from './components/Temperatura';
 function App() {
   return (
     <div className="App">
       <h1>Shop Web - Decorator</h1>
       <Contador></Contador>
       <Listado></Listado>
+      <Temperatura></Temperatura>
     </div>
   );
 }
